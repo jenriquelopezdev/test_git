5b3d4e6db072fd1f59e96b1565876b7565c4ebab
Enrique Lopez <elopez@digitalgeko.com>
2020-04-23 19:04:27 -0600

    ejemplo de columnas responsive

--- javascrypt.js
+++ javascrypt.js
@@ -1,19 +1,2 @@
-
-			$(document).ready(function(){
-
-			var btnClick=true;
-
-		    $("#show").on('click', function() {
-		    	if (btnClick) {
-		    		$(".esconder").show();
-		    		btnClick = !btnClick
-		    	} else {
-		    		$(".esconder").hide();
-		    		btnClick = !btnClick
-		    	}
-		        
-		         return false;
-		      });
-		    });
-		 
-		
+$(document).ready(function() {		
+});
